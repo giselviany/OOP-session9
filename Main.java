6bed75b11df813a38b4bb7325b6276c35fe5c32b
giselviany <121036273+giselviany@users.noreply.github.com>
2022-12-24 12:41:49 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,14 +7,16 @@ public class Main {
 	Scanner scan  = new Scanner(System.in);
 	ArrayList<Food> foodList = new ArrayList<>();
 	
+	int nettPrice = 0;
+	
 	public void orderFood() {
 		String size;
 		String foodType;
-		String dressing;
-		String pastaType;
-		String cheese;
-		String noodleType;
-		String topping;
+		String dressing = null;
+		String pastaType = null;
+		String cheese = null;
+		String noodleType = null;
+		String topping = null;
 		int spicyLevel= 0;
 		int price = 0;
 		
@@ -34,14 +36,18 @@ public class Main {
 			System.out.print("Input your Order [Salad | Pasta | Noodle]: ");
 			foodType = scan.nextLine();
 		} while(!(foodType.equals("Salad") || foodType.equals("Pasta") || foodType.equals("Noodle")));
-
+		
 		if(foodType.equals("Salad")) {
+			price = 35000;
 			do {
 				System.out.print("Input the dressing [Greek Yoghurt || Sweet Mayo || Cilantro Avocado]: ");
 				dressing = scan.nextLine();
 			} while(!(dressing.equals("Greek Yoghurt") || dressing.equals("Sweet Mayo") || dressing.equals("Cilantro Avocado")));
 			
+			foodList.add(new Salad(size, foodType, price, dressing));
+			
 		}else if(foodType.equals("Pasta")) {
+			price = 65000;
 			do {
 				System.out.print("Input pasta type [Spaghetti || Lasagna || Penne]: ");
 				pastaType = scan.nextLine();
@@ -51,7 +57,10 @@ public class Main {
 				cheese = scan.nextLine();
 			} while(!(cheese.equals("Cheddar") || cheese.equals("Parmesan") || cheese.equals("Mozzarella")));
 
+			foodList.add(new Pasta(size, foodType, price, pastaType, cheese));
+			
 		}else if(foodType.equals("Noodle")) {
+			price = 55000;
 			do {
 				System.out.print("Input noodle type [Ramen || Udon || Soba]: ");
 				noodleType = scan.nextLine();
@@ -65,7 +74,14 @@ public class Main {
 				spicyLevel = scan.nextInt();
 				scan.nextLine();
 			} while(spicyLevel < 2 || spicyLevel > 5);
+			
+			foodList.add(new Noodle(size, foodType, price, noodleType, topping, spicyLevel));
 		}
+		
+		System.out.println("succesfully added");
+		System.out.println("Press enter to continue");
+		System.out.println(" ");
+		
 	}
 	
 	public void viewOrder() {
@@ -76,7 +92,66 @@ public class Main {
 			scan.nextLine();
 		}else {
 			System.out.println("===================================================");
-			
+			System.out.println("| NO. |                Food                 | Size |");
+			for(int i=0; i<foodList.size() ; i++) {
+				System.out.print("| ");
+				System.out.print(i+1 + " |");
+				if((foodList.get(i).getFoodType()).equals("Salad")) {
+					System.out.print(" ");
+					System.out.print(foodList.get(i).getFoodType() + " with ");
+					System.out.print(((Salad) foodList.get(i)).getDressing() + " dressing | ");
+				}else if((foodList.get(i).getFoodType()).equals("Pasta")) {
+					System.out.print(" ");
+					System.out.print(((Pasta) foodList.get(i)).getPastaType() + " ");
+					System.out.print(foodList.get(i).getFoodType() + " with ");
+					System.out.print(((Pasta) foodList.get(i)).getCheese() + " cheese | ");
+				}else {
+					System.out.print(" ");
+					System.out.print(((Noodle) foodList.get(i)).getNoodleType() + " ");
+					System.out.print(foodList.get(i).getFoodType() + " with ");
+					System.out.print(((Noodle) foodList.get(i)).getTopping() + " | ");
+				}
+				System.out.print(foodList.get(i).getSize() + " | ");
+				System.out.println(" ");
+			}
+		}
+	}
+	
+	public void payFood() {
+		if(foodList.isEmpty()) {
+			System.out.println("NO DATA!");
+			System.out.println("=========");
+			System.out.println("Press Enter to Continue");
+			scan.nextLine();
+		}else {
+			System.out.println("===================================================");
+			System.out.println("| NO. |                Food                 | Size | Base Price | Total Price |");
+			for(int i=0; i<foodList.size() ; i++) {
+				System.out.print("| ");
+				System.out.print(i+1 + " |");
+				if((foodList.get(i).getFoodType()).equals("Salad")) {
+					System.out.print(" ");
+					System.out.print(foodList.get(i).getFoodType() + " with ");
+					System.out.print(((Salad) foodList.get(i)).getDressing() + " dressing | ");
+				}else if((foodList.get(i).getFoodType()).equals("Pasta")) {
+					System.out.print(" ");
+					System.out.print(((Pasta) foodList.get(i)).getPastaType() + " ");
+					System.out.print(foodList.get(i).getFoodType() + " with ");
+					System.out.print(((Pasta) foodList.get(i)).getCheese() + " cheese | ");
+				}else {
+					System.out.print(" ");
+					System.out.print(((Noodle) foodList.get(i)).getNoodleType() + " ");
+					System.out.print(foodList.get(i).getFoodType() + " with ");
+					System.out.print(((Noodle) foodList.get(i)).getTopping() + " | ");
+				}
+				System.out.print(foodList.get(i).getSize() + " | ");
+				System.out.print(foodList.get(i).getPrice() + " | ");
+				System.out.print(foodList.get(i).generateTotalPrice() + " |");
+				nettPrice = nettPrice + foodList.get(i).generateTotalPrice();
+				System.out.println(" ");
+			} 
+			System.out.println("Total Amount: " + nettPrice);
+			System.out.println(" ");
 		}
 	}
 	
@@ -98,7 +173,7 @@ public class Main {
 			}else if(input == 2) {
 				viewOrder();
 			}else if(input == 3) {
-				//payFood
+				payFood();
 			}
 			
 		}while(input != 4);
@@ -109,3 +184,4 @@ public class Main {
 	}
 
 }
+
