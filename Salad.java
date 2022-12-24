b4c7f47462fce2c6de79339fe9281dfed26a5100
giselviany <121036273+giselviany@users.noreply.github.com>
2022-12-24 12:43:11 +0700

    Update Salad.java

--- Salad.java
+++ Salad.java
@@ -3,8 +3,8 @@ package gslcSession9;
 public class Salad extends Food{
 	private String dressing;
 	
-	public Salad(String size, int price, String dressing) {
-		super(size, price);
+	public Salad(String size, String foodType, int price, String dressing) {
+		super(size, foodType, price);
 		this.dressing = dressing;
 	}
 
@@ -21,5 +21,4 @@ public class Salad extends Food{
 		int totalPrice = (int) (this.getPrice()*1.1);
 		return totalPrice;
 	}
-
 }
